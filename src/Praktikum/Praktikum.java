cf54d8c5716eae55565eb78d04bc8af2029ca9ca
bagasanugrah <33810450+bagasanugrah@users.noreply.github.com>
2020-07-09 15:37:54 +0700

    Add files via upload

---
+++ src/Praktikum/Praktikum.java
@@ -0,0 +1,10 @@
+package Praktikum;
+
+import database.Koneksi;
+public class Praktikum {
+    public static void main(String[] args) {
+        new Koneksi();
+         Koneksi koneksi = new Koneksi();
+    }
+
+}
